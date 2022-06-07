c189ebc83091f165e2fae52c9a06cd7dc67dbae0
20108539-AUT <82435505+Seongrok-Shin@users.noreply.github.com>
2022-06-08 01:10:05 +1200

    Implement java file
    -add java folders

---
+++ src/Question1/FileSorter.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Question1;
+
+/**
+ *
+ * @author ssr7324
+ * @param <E>
+ */
+public class FileSorter<E> {
+    private int limit;
+    private 
+}
