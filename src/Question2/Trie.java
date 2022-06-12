f269418a213d91b97f3d98cab2055df3fbf464c5
20108539-AUT <82435505+Seongrok-Shin@users.noreply.github.com>
2022-06-12 20:24:52 +0900

    Update Trie.java

--- src/Question2/Trie.java
+++ src/Question2/Trie.java
@@ -6,7 +6,9 @@
 package Question2;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -17,10 +19,17 @@ public class Trie {
 
     private class TrieNode {
 
-        private final Map<String, TrieNode> children = new HashMap<>();
+        private final Map<Character, TrieNode> children;
         private boolean isLeaf;
+        private char element;
 
-        public Map<String, TrieNode> getChildren() {
+        public TrieNode() {
+            children = new HashMap<>();
+            isLeaf = false;
+            element = '\u0000';
+        }
+
+        public Map<Character, TrieNode> getChildren() {
             return children;
         }
 
@@ -33,10 +42,68 @@ public class Trie {
         }
     }
 
-    TrieNode root;
+    private TrieNode root;
 
     public Trie() {
         root = new TrieNode();
+    }
+
+    public boolean add(String element) {
+        TrieNode currentNode = root;
+
+        for (char character : element.toCharArray()) {
+            currentNode = currentNode.getChildren().computeIfAbsent(character, c -> new TrieNode());
+        }
+
+        currentNode.setLeaf(true);
+        return currentNode.isLeaf;
+    }
+
+    public boolean remove(String element) {
+        TrieNode currentNode = root;
+
+        for (char character : element.toCharArray()) {
+            currentNode = currentNode.getChildren().computeIfAbsent(character, c -> null);
+        }
+
+        currentNode.setLeaf(false);
+        return currentNode.isLeaf;
+    }
+
+    public boolean contains(String prefix) {
+        TrieNode currentNode = root;
+
+        for (char character : prefix.toCharArray()) {
+
+            TrieNode next = currentNode.getChildren().get(character);
+
+            if (Objects.isNull(next)) {
+                return false;
+            }
+
+            currentNode = next;
+        }
+
+        return true;
+    }
+
+    public boolean removeAll(String prefix) {
+
+        return true;
+    }
+
+    public boolean startsWith(String prefix) {
+        if (contains(prefix) != false) {
+
+            return true;
+        } else {
+
+            return false;
+        }
+    }
+
+    public boolean suggestions(String prefix) {
+        return true;
     }
 
     @Override
@@ -56,7 +123,7 @@ public class Trie {
             for (Character c : chars) {
                 TrieNode child = current.children.get(c);
                 levelString += tabs + " [" + c + "]";
-                if (child.element != null) {
+                if (Objects.nonNull(child.element)) {
                     levelString += " >> " + child.element;
                 }
                 levelString += "\n";
@@ -66,4 +133,15 @@ public class Trie {
         return levelString;
     }
 
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.add("tea");
+        trie.add("to");
+        trie.add("him");
+        trie.add("he");
+        trie.add("hit");
+        trie.add("her");
+
+        System.out.println(trie);
+    }
 }
