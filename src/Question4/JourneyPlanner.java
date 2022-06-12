2fc8a4bd9607b3358e77030fbd93e0345b3ebf99
20108539-AUT <82435505+Seongrok-Shin@users.noreply.github.com>
2022-06-12 17:28:04 +0900

    Update JourneyPlanner.java

--- src/Question4/JourneyPlanner.java
+++ src/Question4/JourneyPlanner.java
@@ -6,10 +6,8 @@
 package Question4;
 
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
